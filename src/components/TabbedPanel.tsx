f41c817ac58437985e1b2d19c9124b7f36913ea1
Nicholas Sorokin <nicholas.sorokin@hotmail.com>
2022-04-23 15:45:58 +0930

    feat: add TabbedPanel

---
+++ src/components/TabbedPanel.tsx
@@ -0,0 +1,45 @@
+import React, { cloneElement, useState } from 'react';
+import Button from './Button';
+import './TabbedPanel.css';
+
+function TabbedPanel<
+	TTabs extends Record<string, React.ReactElement<{ className?: string }>>
+>({
+	children,
+	defaultTab,
+	className,
+}: {
+	children: TTabs;
+	defaultTab: keyof TTabs;
+	className?: string;
+}) {
+	const [activeTab, setActiveTab] = useState<keyof TTabs>(defaultTab);
+
+	className = className ? `${className} TabbedPanel` : 'TabbedPanel';
+
+	return (
+		<div className={className}>
+			<div className="TabbedPanel__Tabs">
+				{Object.keys(children).map((tab) => (
+					<Button
+						className={`TabbedPanel__Tab ${
+							tab === activeTab ? 'TabbedPanel__Tab--active' : ''
+						}`}
+						key={tab}
+						onClick={() => setActiveTab(tab as keyof TTabs)}
+					>
+						{tab}
+					</Button>
+				))}
+			</div>
+
+			{cloneElement(children[activeTab], {
+				className: children[activeTab].props.className
+					? `${children[activeTab].props.className} TabbedPanel__Content`
+					: 'TabbedPanel__Content',
+			})}
+		</div>
+	);
+}
+
+export default TabbedPanel;
